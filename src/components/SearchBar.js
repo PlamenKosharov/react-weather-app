cbc13701d23fe2f4b6043fcbf870ac704839049d
Plamen <plamenkosharov777@gmail.com>
2025-10-19 21:41:33 +0300

    Added searchbar functionality for fetching city weather

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,35 @@
 import styles from "../styles/searchbar.module.css"
+import {useState} from "react";
 
 export default function SearchBar() {
+  const [searchText, setSearchText] = useState("");
+  const [cityData, setCityData] = useState("");
+
+  async function handleSearch() {
+    if (!searchText) {
+      alert("Please enter a city name.");
+      return;
+    }
+    try{
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${searchText}&appid=d457f9a1531004669ebc5a64b33dcf3c&units=metric`)
+      if (!response.ok){
+        throw new Error("No such city.");
+      }
+      const data = await response.json();
+      setCityData(data);
+    }
+    catch(err){
+      console.log(err.message);
+    }
+  }
+
   return(
     <div className={styles.searchBarContainer}>
-      <input className={styles.searchBar} type={"text"} placeholder={"Enter city..."}/>
-      <img className={styles.searchImg} alt = "search" src = "/images/search.png"/>
+      <input className={styles.searchBar} type={"text"} placeholder={"Enter city..."}
+             value={searchText}
+             onChange={(e) => setSearchText(e.target.value)}
+      />
+      <img onClick={handleSearch} className={styles.searchImg} alt = "search" src = "/images/search.png"/>
     </div>
   )
 }
